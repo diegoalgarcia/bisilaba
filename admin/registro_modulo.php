16eb8b2cea61c751081a1f58b8a252b18ee06eac
“dagarciaag” <dagarciaag@ut.edu.co>
2024-06-02 22:16:41 -0500

    archivo que ingresa a la tabla modulos los titulos de los textos como rojo verde o azul

--- admin/registro_modulo.php
+++ admin/registro_modulo.php
@@ -5,23 +5,14 @@ include("conexion.php");
     // este archivo ingresa los usuarios administrativos
     $nombre=$_POST['nombre'];                            
     $titulo=$_POST['titulo'];  
-	$autor=$_POST['autor'];         
-    $disenio=$_POST['disenio'];	      
-	$ilustracion=$_POST['ilustracion'];          
-    $editor=$_POST['editor'];    
-    $anio=$_POST['anio'];
-    $isbm=$_POST['isbm']; 
-    $paginas=$_POST['paginas'];   
-    $formato=$_POST['formato'];   
-    $contenido=$_POST['contenido'];  
-
+	 
     $sql="INSERT INTO modulo 
-          (nombre, titulo, autor, disenio, ilustracion, editor, anio, ISBN, paginas, formato, contenido)
-    VALUES(:miNom, :miTitu, :miAut, :miDisenio, :miIlust, :miEdit, :miAni, :miIsb, :miPagi, :miForm, :miCont)";
+          (nombre, titulo)
+    VALUES(:miNom, :miTitu)";
     
    $resultado=$base->prepare($sql);
 
-$resultado->execute(array(":miNom"=>$nombre, ":miTitu"=>$titulo, ":miAut"=>$autor, ":miDisenio"=>$disenio, ":miIlust"=>$ilustracion, ":miEdit"=>$editor, ":miAni"=>$anio, ":miIsb"=>$isbm, ":miPagi"=>$paginas, ":miForm"=>$formato, ":miCont"=>$contenido));
+$resultado->execute(array(":miNom"=>$nombre, ":miTitu"=>$titulo));
     /* 
      $mensaje='error';
      header("location:form_registro.php?mensaje=$mensaje");*/
