3ad132b09802f510e65ff2c908c86e83885c9967
“dagarciaag” <dagarciaag@ut.edu.co>
2024-06-04 03:22:29 -0500

    se cambio el nombre al archivo de registro_modulos a registro_libros

---
+++ admin/registro_libros.php
@@ -0,0 +1,21 @@
+<?php 
+
+include("conexion.php");
+ 
+    // este archivo ingresa los usuarios administrativos
+    $nombre=$_POST['nombre'];                            
+    $titulo=$_POST['titulo'];  
+	 
+    $sql="INSERT INTO libros 
+          (nombre, titulo)
+    VALUES(:miNom, :miTitu)";
+    
+   $resultado=$base->prepare($sql);
+
+$resultado->execute(array(":miNom"=>$nombre, ":miTitu"=>$titulo));
+    /* 
+     $mensaje='error';
+     header("location:form_registro.php?mensaje=$mensaje");*/
+     header("location:lista_modulos.php");
+     echo "Exito al ingresar el registro de Modulo...";
+?>
