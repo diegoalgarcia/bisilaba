de4eb92efbb9afd956da72e92f62102cb5dea751
“dagarciaag” <dagarciaag@ut.edu.co>
2024-06-07 11:42:18 -0500

    se crean archivos para el ingreso de items a la tabla de la bd

---
+++ admin/ingreso_item.php
@@ -0,0 +1,21 @@
+<?php 
+
+include("conexion.php");
+ 
+    // este archivo ingresa los usuarios administrativos
+   // $nombre=$_POST['nombre'];                            
+    $titulo=$_POST['titulo'];  
+	 
+    $sql="INSERT INTO item 
+          (titulo)
+    VALUES(:miTitu)";
+    
+   $resultado=$base->prepare($sql);
+
+$resultado->execute(array(":miTitu"=>$titulo));
+    /* 
+     $mensaje='error';
+     header("location:form_registro.php?mensaje=$mensaje");*/
+     header("location:lista_items_portada.php");
+     echo "Exito al ingresar el registro de Modulo...";
+?>
