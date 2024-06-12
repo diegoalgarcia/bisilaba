0d27109725b3d14be18c4217a80eed16cfb36426
“dagarciaag” <dagarciaag@ut.edu.co>
2024-06-11 21:34:47 -0500

    son archivos consecutivos de asociacion de registros a la portada del libro en ingreso_pdf_a_portada.php hace falta llenar el campo estado de la tabla ruta_pdf con el fin de validar que el item se pueda deshabilitar para que no se pueda utilizar para otro titulo

---
+++ admin/ingreso_pdf_a_portada.php
@@ -0,0 +1,46 @@
+<?php
+   //select * from archivos where nombre like "%busca%"
+
+   /* Este archivo recibe los parametros e ingresa en la bd tabla 
+   la ruta de archivos archivo pdf
+   sube a la carpeta correspondiente del host los pdf seleccionados   
+   */
+   include("conexion.php");
+
+   $texto_enlace=$_POST['texto'];  
+
+   $directorio = "uploads/";
+   $archivo = $directorio . basename($_FILES["archivo"]["name"]);
+   $archivo2 = $directorio . basename($_FILES["archivo"]["name"]);
+   $archivo = utf8_decode($archivo);
+
+   if($_FILES['archivo']['type']=='application/pdf'){
+		
+    move_uploaded_file($_FILES["archivo"]["tmp_name"], $archivo);
+      
+    $sql="INSERT INTO pdf_ruta (texto_enlace, ruta)	
+    VALUES (:miText, :miRuta)";
+      
+      $resultado=$base->prepare($sql);
+
+      $resultado->execute(array(":miText"=>$texto_enlace, ":miRuta"=>$archivo2));
+      
+      header("location:lista_doc_pdf.php");
+    }else {
+      echo 'No se pudo cargar el pdf :(';
+    //  header("location:lista_contenido.php");
+    }
+ 
+   /* 
+    Pagina de los siguientes lineas
+   https://datoweb.com/post/2952/como-validar-archivos-pdf-con-php
+
+   video 01
+    https://youtu.be/GSoFP4HPKvM
+
+
+    video 02
+    Subir Archivos con PHP - APRENDES SÍ O SÍ
+    https://youtu.be/X85_BbFgK8s
+   */
+?>
