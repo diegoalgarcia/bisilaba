02a6dbab14104a69d7e96da5f81ac4599a4ae214
“dagarciaag” <dagarciaag@ut.edu.co>
2024-08-21 21:15:16 -0500

    modulos que contienen el header navegacion y pie de la pagina

---
+++ vistas/encabezado.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Educate Html 5 Template">
+
+  <title>Helise || LMS Herramienta Educativa</title>
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="./plantillas/image/x-icon" href="./plantillas/assets/media/favicon.png">
+
+  <!-- All CSS files -->
+  <link rel="stylesheet" href="./plantillas/assets/vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./plantillas/assets/vendor/fontawesome/font-awesome.css">
+  <link rel="stylesheet" href="./plantillas/assets/vendor/slickslider/slick.css">
+  <link rel="stylesheet" href="./plantillas/assets/vendor/jquery-magnific-popup/jquery.magnific-popup.css">
+  <link rel="stylesheet" href="./plantillas/assets/vendor/animate/animate.min.css">
+  <link rel="stylesheet" href="./plantillas/assets/css/app.css">
+
+</head>
+
+<body class="custom-cursor locked">
+
+  <!-- cursor style  -->
+  <div class="custom-cursor__cursor"></div>
+  <div class="custom-cursor__cursor-two"></div>
+
+  <!-- Preloader-->
+  <div id="preloader">
+    <div class="book">
+      <div class="inner">
+        <div class="left"></div>
+        <div class="middle"></div>
+        <div class="right"></div>
+      </div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+
+  <!-- Main Wrapper Start -->
+  <div id="main-wrapper" class="main-wrapper">
+    <!-- Header Area start -->
+    <header>
+      <nav class="main-menu">
+        <div class="container">
+          <div class="main-menu__block">
+            <div class="main-menu__left">
+              <div class="main-menu__logo">
+                <a href="index.php">
+                  <img src="./plantillas/assets/media/logo.png" alt="Educate">
+                </a>
+              </div>
+              <div class="main-menu__nav">
+                <ul class="main-menu__list">
+                  <li class="dropdown">
+                    <a href="javascript:void(0);" class="active">Inicio</a>
+                    <ul>
+                      <li><a href="index.php">Inicio</a></li>
+                      
+                    </ul>
+                  </li>
+                  <li class="dropdown">
+                    <a href="javascript:void(0);">Módulos</a>
+                    <ul class="sub-menu">
+                      <li class="dropdown">
+                          
+                        <li><a href="?c=listinicial"></a>Modulo Rojo</li>
+                          
+                      </li>
+                      
+                  </ul>
+                  </li>
+                  <li><a href="helise.php">HELISE</a></li>
+                  
+                  <li class="dropdown ">
+                    <a href="javascript:void(0);">Plan Lector</a>
+                    <ul>
+                      <li><a href="about.php">Nosotros</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="contact.php">Cuestionarios tipo saber</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="main-menu__right">
+              <a href="#" class="main-menu__search search-toggler">
+                <i class="fal fa-search"></i>
+              </a>
+              <a href="form_login.php" class="main-menu__login">
+                <i class="fal fa-user"></i>
+              </a>
+              <a href="contact.php" class="educate-btn sm d-xl-flex d-none"><span
+                  class="educate-btn__curve"></span>Contactenos</a>
+              <a href="#" class="main-menu__toggler mobile-nav__toggler">
+                <i class="fa fa-bars"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- Sticky Header -->
+    <div class="stricky-header stricked-menu main-menu">
+      <div class="sticky-header__content"></div>
+    </div>
+    <!-- Header Area End  -->
---
+++ vistas/pie.php
@@ -0,0 +1,148 @@
+<!-- Footer Area Start -->
+<footer>
+      <div class="footer_main py-60">
+        <img src="./plantillas/assets/media/shapes/vector-7.png" alt="" class="vector_shape">
+        <img src="./plantillas/assets/media/shapes/dots-1.png" alt="" class="dots">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-3 ">
+              <div class="footer_widget">
+                <a href="index.html" class="mb-8"><img src="./plantillas/assets/media/logo.png" alt=""></a>
+                <p class="description_text">Ofrece una propuesta pedagogica fundamentada
+                   en actividades encaminadas al desarrollo o 
+                   fortalecimiento de las habilidades requeridas para 
+                   llevar a cabo una Lectura Integral, Secuencial y 
+                   Estructurada.
+                </p>
+              </div>
+            </div>
+            <div class="col-xl-2 col-lg-3 col-sm-6">
+              <div class="footer_widget">
+                <h5 class="medium-black mb-16">Enlaces</h5>
+                <ul class="unstyled list">
+                  <li><a href="">Módulos de lectura</a></li>
+                  <li><a href="">HELISE</a></li>
+                  <li><a href="">Plan Lector</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-xl-2 col-lg-3 col-sm-6">
+              <div class="footer_widget">
+                <h5 class="medium-black mb-16">Explorar</h5>
+                <ul class="unstyled list">
+                  <li><a href="about.html">Nosotros</a></li>
+                  <li><a href="gallery.html">Galeria</a></li>
+                  <li><a href="team-detail.html">Cuestionarios tipo saber</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-xl-2 col-lg-3 col-sm-6">
+              <div class="footer_widget">
+                <h5 class="medium-black mb-16">Información</h5>
+                <ul class="unstyled list">
+                  <li><a href="login.php">Login</a></li>
+                  <li><a href="register.php">Register</a></li>
+                  <li><a href="contact.php">Contactenos</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+              <div class="footer_widget">
+                <h5 class="medium-black mb-16">Contactenos</h5>
+                <ul class="unstyled list">
+                  <li class="mb-16"><i class="fal fa-map-marker-alt"></i>Ibagué. Tolima</li>
+                  <li class="mb-16"><a href="mailto:info@example.com"><i
+                        class="fal fa-envelope"></i>email@example.com</a></li>
+                  <li><a href="tel:123456789"><i class="fal fa-phone-alt"></i>+1 234 567 890</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="bottom-row">
+            <ul class="unstyled social_icons_list">
+              <li><img src="./plantillas/assets/media/icons/brands/Instagram.png" alt=""></li>
+              <li><img src="./plantillas/assets/media/icons/brands/Facebook.png" alt=""></li>
+              <li><img src="./plantillas/assets/media/icons/brands/Twitter.png" alt=""></li>
+              <li><img src="./plantillas/assets/media/icons/brands/Linkedin.png" alt=""></li>
+            </ul>
+            <div class="newsletter_block">
+              <h5>Newsletter</h5>
+              <form class="newsletter" action="index-2.html">
+                <input type="email" class="form-control" required placeholder="Your email">
+                <button type="submit" class="educate-btn sm"><span class="educate-btn__curve"></span>Subscribirse</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copyright_row">
+        <p>©2024 HELISE All Rights Reserved. Gestión de contenido: Ing Diego A. Gacía.</p>
+      </div>
+    </footer>
+    <!-- Footer Area End -->
+  </div>
+
+  <!-- Mobile Menu Start -->
+  <div class="mobile-nav__wrapper">
+    <div class="mobile-nav__overlay mobile-nav__toggler"></div>
+    <div class="mobile-nav__content">
+      <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
+      <div class="logo-box">
+        <a href="index.php" aria-label="logo image"><img src="./plantillas/assets/media/logo-light.png" alt="educate"></a>
+      </div>
+      <div class="mobile-nav__container"></div>
+      <ul class="mobile-nav__contact list-unstyled">
+        <li>
+          <i class="fas fa-envelope"></i>
+          <a href="mailto:example@company.com">helise@recursoseducativos.com</a>
+        </li>
+        <li>
+          <i class="fa fa-phone-alt"></i>
+          <a href="tel:+12345678">+123 (4567) -890</a>
+        </li>
+      </ul>
+      <div class="mobile-nav__social">
+        <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.pinterest.com/"><i class="fab fa-pinterest-p"></i></a>
+        <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+      </div>
+    </div>
+  </div>
+  <!-- Mobile Menu End -->
+
+  <div class="search-popup">
+    <div class="search-popup__overlay search-toggler"></div>
+    <div class="search-popup__content">
+      <form role="search" method="get" class="search-popup__form" action="index.html">
+        <input type="text" id="search" placeholder="Search Here...">
+        <button type="submit"><i class="fal fa-search"></i></button>
+      </form>
+    </div>
+  </div>
+  <!-- search-popup -->
+
+  <!-- back-to-top-start -->
+  <a href="#" class="scroll-top">
+    <svg class="scroll-top__circle" width="100%" height="100%" viewBox="-1 -1 102 102">
+      <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    </svg>
+  </a>
+  <!-- back-to-top-end -->
+
+  <!-- Jquery Js -->
+  <script src="./plantillas/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="./plantillas/assets/vendor/jquery/jquery-3.6.3.min.js"></script>
+  <script src="./plantillas/assets/vendor/jquery-appear/jquery-appear.js"></script>
+  <script src="./plantillas/assets/vendor/jquery-validator/jquery-validator.js"></script>
+  <script src="./plantillas/assets/vendor/countdown/jquery.countdown.min.js"></script>
+  <script src="./plantillas/assets/vendor/tilt/tilt.jquery.js"></script>
+  <script src="./plantillas/assets/vendor/jquery-magnific-popup/jquery.magnific-popup.min.js"></script>
+  <script src="./plantillas/assets/vendor/slickslider/slick.min.js"></script>
+  <script src="./plantillas/assets/vendor/wow/wow.js"></script>
+
+  <!-- Site Scripts -->
+  <script src="./plantillas/assets/js/app.js"></script>
+</body>
+
+</html>
