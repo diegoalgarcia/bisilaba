bf9c45f1661cfb31da4b42822d02928edcf85961
“dagarciaag” <dagarciaag@ut.edu.co>
2024-08-21 21:17:05 -0500

    seccion del cuerpo de la pagina home

---
+++ vistas/inicio/principal.php
@@ -0,0 +1,167 @@
+    <!-- Hero Banner Start -->
+    <section class="hero-banner-2">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-6 col-lg-8">
+            <div class="text_block">
+              <h1 class="hero_title mb-16">!Herramienta Educativa para una <span>Lectura Integral <img src="./plantillas/assets/media/shapes/line-vector-2.png"
+                    alt="" class="line_vector"></span> Secuencial y Estructurada!</h1>
+              <p class="mb-48">!...Hacia una Lectura con Èxito!<br> 
+                <!--aliquam tempus. Volutpat tortor tincidunt egestas sit risus donec.--></p>
+              <div class="btn-block">
+                <a href="contact.html" class="educate-btn sec"><span class="educate-btn__curve"></span>Contantenos</a>
+                <a href="courses.html" class="educate-btn sec-2"><span class="educate-btn__curve"></span>Descrubrir los Modulos</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="img_block">
+          <div>
+            <img src="./plantillas/assets/media/banners/hero-banner-2/main-img.png" alt="" class="banner_main_img">
+          </div>
+          <a href="https://www.youtube.com/watch?v=8nXX1WOuvrk" class="play-btn video-popup"><i
+              class="fas fa-play"></i></a>
+          <!--
+          <div class="banner_feature_card">
+            <div class="card_block">
+              <h6 class="mb-4p">2K Courses</h6>
+              <p>Based on Latest Knowledge</p>
+              <img src="assets/media/users/bu.png" alt="" class="user_placeholder">
+            </div>
+          </div>
+          -->
+        </div>
+        <div class="row">
+          <div class="col-xxl-6 col-xl-8 col-lg-10">
+            <div class="row features_row">
+              <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="200ms">
+                <div class="feature__card">
+                  <div class="feature__icon">
+                    <img src="./plantillas/assets/media/icons/Support.png" alt="">
+                  </div>
+                  <div class="feature__content">
+                    <h5 class="mb-8">Recursos Descargables</h5>
+                    <p>Hemos seleccionados los mejores recursos para entrenar las competencias lectoras</p>
+                    <img src="./plantillas/assets/media/shapes/feture-bg-shape.png" alt="" class="feature-bg-shape">
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="400ms">
+                <div class="feature__card">
+                  <div class="feature__icon">
+                    <img src="./plantillas/assets/media/icons/Check-mark.png" alt="">
+                  </div>
+                  <div class="feature__content">
+                    <h5 class="mb-8">Procesos de Pensamiento</h5>
+                    <p>Memoria, razonamiento, aplicación, captación, comprensión, análisis, síntesis, interpretación, crítica y creatividad.</p>
+                    <img src="./plantillas/assets/media/shapes/feture-bg-shape.png" alt="" class="feature-bg-shape">
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 d-md-block d-none wow fadeInUp" data-wow-delay="600ms">
+                <div class="feature__card">
+                  <div class="feature__icon">
+                    <img src="./plantillas/assets/media/icons/Quality.png" alt="">
+                  </div>
+                  <div class="feature__content">
+                    <h5 class="mb-8">Lectura Integral, Secuencial y Estructurada</h5>
+                    <p>Seleccion de las mejores actividades destinadas a mejorar la RAPIDEZ VISUAL, la LÓGICA LINGÜÍSTICA, la LÓGICA MATEMÁTICA y la COMPRENSIÓN TEXTUAL.</p>
+                    <img src="./plantillas/assets/media/shapes/feture-bg-shape.png" alt="" class="feature-bg-shape">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Hero Banner End -->
+
+    <!-- About Area Start --
+    !-- About Area End -->
+
+    <!-- Brands Area Start --
+    !-- Brands Area End -->
+
+    <!-- Courses Category Area Start -->
+    <section class="py-60 ">
+      <div class="container">
+        <div class="section-title mb-48">
+          <div class="heading">
+            <h6 class="color-primary mb-8">–––– Descubre nuestros Módulos</h6>
+            <h2>TALLERES <br> DE <span class="fm-sec">LECTURA COMPRENSIVA</span></h2>
+          </div>
+          <a href="contenido_mod_rojo.php" class="educate-btn d-md-block d-none"><span class="educate-btn__curve"></span>View
+            All</a>
+        </div>
+        <div class="row">
+          <div class="col-xl-4 col-md-6 wow fadeInUp" data-wow-delay="150ms">
+            <div class="course__category__card_2 mb-24">
+              <img src="./plantillas/assets/media/courses/categories/c2-2.png" alt="" class="image">
+              <div class="course__category__card_text">
+                <a href="contenido_mod_rojo.php" class="course__category__card_title h4 mb-16">Módulo Rojo</a>
+                <p class="category_info_text">Este texto es una propuesta pedagógica
+                   que conduce al estudiante hacia la adquisición de 
+                   conocimientos básicos sobre LA PALABRA </p>
+                <ul class="category__info p-0 mb-8">
+                  <li><i class="fas fa-star"></i>EISE</li>
+                  <li><i class="fal fa-graduation-cap"></i>128 Páginas</li>
+                  <li><i class="fal fa-clock"></i>2018+</li>
+                </ul>
+                <a href="contenido_mod_rojo.php" class="educate-btn sec"><span class="educate-btn__curve"></span>Explorar Ahora</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-4 col-md-6 wow fadeInUp" data-wow-delay="300ms">
+            <div class="course__category__card_2 mb-24">
+              <img src="./plantillas/assets/media/courses/categories/c2-1.png" alt="" class="image">
+              <div class="course__category__card_text">
+                <a href="contenido_mod_rojo.php" class="course__category__card_title h4 mb-16">Módulo Azul</a>
+                <p class="category_info_text">Son 47 textos con más de 500 
+                  preguntas Tipo SABER.</p>
+                <ul class="category__info p-0 mb-8">
+                  <li><i class="fas fa-star"></i>EISE</li>
+                  <li><i class="fal fa-graduation-cap"></i>132 Páginas</li>
+                  <li><i class="fal fa-clock"></i>2018+</li>
+                </ul>
+                <a href="contenido_mod_rojo.php" class="educate-btn sec"><span class="educate-btn__curve"></span>Explorar Ahora</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-4 col-md-6 wow fadeInUp" data-wow-delay="450ms">
+            <div class="course__category__card_2 mb-24">
+              <img src="./plantillas/assets/media/courses/categories/c2-6.png" alt="" class="image">
+              <div class="course__category__card_text">
+                <a href="contenido_mod_rojo.php" class="course__category__card_title h4 mb-16">Módulo Verde</a>
+                <p class="category_info_text">Son actividades referidas a la 
+                  RAPIDEZ VISUAL, la LÓGICA LINGÜÍSTICA, la LÓGICA 
+                  MATEMÁTICA y la COMPRENSIÓN TEXTUAL.</p>
+                <ul class="category__info p-0 mb-8">
+                  <li><i class="fas fa-star"></i>EISE</li>
+                  <li><i class="fal fa-graduation-cap"></i>134 Páginas</li>
+                  <li><i class="fal fa-clock"></i>2018+</li>
+                </ul>
+                <a href="contenido_mod_rojo.php" class="educate-btn sec"><span class="educate-btn__curve"></span>Explorar Ahora</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Courses Category Area End -->
+
+    <!-- Courses Area Start --
+    !-- Courses Area End -->
+
+    <!-- Team Area Start --
+    !-- Team Area End -->
+
+    <!-- Discount Banner Area Start --
+    !-- Discount Banner Area End -->
+
+    <!-- Testimonials Area Start --
+    
+    !-- Testimonials Area End -->
+
+    <!-- Blogs Area Start --
+    !-- Blogs Area End -->
